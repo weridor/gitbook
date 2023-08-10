450ad65541b34ae51914600acc298b9a52ffcee5
屿 <1365797108@qq.com>
2023-08-10 10:45:28 +0800

    first

---
+++ .vuepress/config.js
@@ -0,0 +1,56 @@
+import {defineUserConfig} from 'vuepress';
+import {defaultTheme} from '@vuepress/theme-default';
+import book from '../book.json';
+import {googleAnalyticsPlugin} from "@vuepress/plugin-google-analytics";
+import {resolve} from "path";
+import {registerComponentsPlugin} from "@vuepress/plugin-register-components";
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: book.title,
+  description: book.description,
+  base: '/gitbook/',
+  head: [],
+  theme: defaultTheme({
+    docsBranch: 'master',
+    navbar: [
+      {
+        text: '我的博客',
+        link: 'http://120.46.170.23/',
+      },
+      {
+        text: '我的作品',
+        link: 'https://github.com/weridor',
+      },
+      {
+        text: 'Bilibili',
+        link: 'https://space.bilibili.com/440586446/',
+      }
+    ],
+    repo: 'weridor/gitbook',
+    sidebar: [
+      {
+        'text': '前言',
+        'link': '/',
+      },
+      'interview_basic.md',
+      'oop-design-pattern.md',
+      'factory-pattern.md',
+      'adapter-pattern.md',
+      'facade-pattern.md',
+      'composite-pattern.md',
+      'flyweight-pattern.md',
+      'end.md',
+    ],
+  }),
+  plugins: [
+    googleAnalyticsPlugin({
+      id: 'G-6X03SBJR88',
+    }),
+    registerComponentsPlugin({
+      components: {
+        Adsense: resolve(__dirname, './components/adsense.vue'),
+      },
+    }),
+  ],
+});
